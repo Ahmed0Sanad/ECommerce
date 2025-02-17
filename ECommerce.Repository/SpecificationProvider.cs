be7e828d3e41378b8f522a9927a76fad5622d985
Ahmed Sanad <ahmedsanad880@gmail.com>
2025-02-17 18:43:15 +0200

    Specifications

---
+++ ECommerce.Repository/SpecificationProvider.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Core;
+using ECommerce.Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Repository
+{
+    public static class SpecificationProvider<T> where T :BaseEntity
+    {
+        public static IQueryable<T> GetQuary(IQueryable<T> intputQuary , ISpecification<T> spec)
+        {
+            var quary = intputQuary;
+            if (spec.criteria != null) 
+            {
+                quary= quary.Where(spec.criteria);
+            
+            }
+            quary = spec.includes.Aggregate(quary, (q, s) => q.Include(s));
+            return quary;
+        }
+    }
+}
---
+++ E_Commerce/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using ECommerce.Core;
+using ECommerce.Core.Repository.Contract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IGenericRepository<Product> _productRepo;
+
+        public ProductController(IGenericRepository<Product> ProductRepo)
+        {
+            _productRepo = ProductRepo;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Getall() 
+        {
+            var result =await _productRepo.GetAllAsync();
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _productRepo.GetAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+    }
+}
